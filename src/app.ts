58a472d18a90c5489509089d0066312c6259b6b0
sujaltangde <sujaltagade01@gmail.com>
2025-06-09 01:04:35 +0530

    feat: added health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/identify', identifyRouter);
 
-
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Server is healthy 🚀' });
+});
 
 
 export default app;
